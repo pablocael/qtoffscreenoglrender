62dc03fdfd08adfeae8fd505cbbd11ba4e41b8b5
Pablo Carneiro Elias <pablo.cael@gmail.com>
2020-06-07 13:51:46 +0900

    initial import

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <QGuiApplication>
+#include <QOffscreenSurface>
+#include <QOpenGLContext>
+#include <QOpenGLFramebufferObject>
+#include <QImage>
+#include <QOpenGLFunctions_3_3_Core>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QSurfaceFormat format;
+    format.setSamples(16);
+    QSurfaceFormat::setDefaultFormat(format);
+
+    QOpenGLContext context;
+    if (!context.create())
+        qFatal("Failed to create OpenGL context");
+
+    // Must have a "surface". Depending on the platform this may be a small
+    // window, pbuffer surface, or even nothing (in case surfaceless contexts are
+    // supported) under the hood.
+    QOffscreenSurface surface;
+    surface.create();
+
+    if (!context.makeCurrent(&surface))
+        qFatal("makeCurrent failed");
+
+    const QSize size(1280, 800);
+    QOpenGLFramebufferObject fbo(size, QOpenGLFramebufferObject::CombinedDepthStencil);
+
+
+    glClear(GL_COLOR_BUFFER_BIT
+          | GL_DEPTH_BUFFER_BIT
+          | GL_STENCIL_BUFFER_BIT);
+
+    glViewport(0,0,100,100);
+    glOrtho(0,100,0,100,0,1);
+
+    glColor3f(1.0,0.0,1.0);
+
+    glBegin(GL_LINE_STRIP);
+        glVertex2f(10.0f,10.0f);
+        glVertex2f(90.0f,10.0f);
+        glVertex2f(90.0f,90.0f);
+        glVertex2f(90.0f,10.0f);
+    glEnd();
+    // Write it to a file.
+    context.makeCurrent(&surface);
+
+
+    fbo.toImage().save("/Users/pabloelias/Documents/Development/TestOpenGLQt/test.png");
+
+
+    return 0;
+}
